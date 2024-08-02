a23920442029c4559329792db2350f4094ea9b40
idkmate11 <114446050+idkmate11@users.noreply.github.com>
2024-08-02 16:16:41 +0200

    Solved my 2nd exercise. It took me around 20-30 minutes, but i'm happy cuz i was able to solve it by myself

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  for (let i = 0; i < num; i++) {
+    return string.repeat(num);
+  }
+  if (num < 0) return "ERROR";
+  if (num === 0) return string.repeat(num);
 };
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
