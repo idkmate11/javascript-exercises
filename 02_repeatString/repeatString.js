cae5548fcfdc4bcbbea0ef4589959ebfe9b8215f
idkmate11 <114446050+idkmate11@users.noreply.github.com>
2024-08-02 16:30:23 +0200

    Adjusted the code a lil bit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,8 @@
 const repeatString = function (string, num) {
-  for (let i = 0; i < num; i++) {
-    return string.repeat(num);
-  }
   if (num < 0) return "ERROR";
   if (num === 0) return string.repeat(num);
+
+  return string.repeat(num);
 };
 repeatString("hey", 3);
 
