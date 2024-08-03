cda92384d9c8930f47053a0ce7cbfabd950634d0
idkmate11 <114446050+idkmate11@users.noreply.github.com>
2024-08-03 23:35:11 +0200

    This one was hard. But i'm glad i finished it

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (...anArray) {
+  let newArray = [];
+  let excludedArrays = anArray.slice(1);
+
+  for (number of anArray[0]) {
+    if (!excludedArrays.includes(number)) {
+      newArray.push(number);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
